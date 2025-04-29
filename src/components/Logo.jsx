e1aae97486e883d37810a1ea898ca4666628a9b7
BrandonPu <bpu-2023395@Kinal.edu.gt>
2025-04-28 20:29:53 -0600

    Se agrego el Login (solo la base)

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/img/Login.png'
+
+export const Logo = ({ text }) => {
+    return (
+        <div className="auth-form-logo-container">
+            <img src={logo} alt="Login" />
+            <span>{text}</span>
+        </div>
+    )
+}
